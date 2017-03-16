82db4d76e1aa0ca50f3010962e796165068030bd
Boris <brankovic.boris.bb@gmail.com>
2017-03-16 08:18:23 +0100

    Created maven-based folder structure for tests.

---
+++ src/main/java/rsa/example/RsaTestApplication.java
@@ -0,0 +1,12 @@
+package rsa.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsaTestApplication.class, args);
+	}
+}
